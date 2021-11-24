586e43f078eb26daeb15a1f20100353112ce5997
Paris Kapsouros <savah@users.noreply.github.com>
2021-11-24 14:27:00 +0300

    Update script-mcommerce-ma.js

--- js/script-mcommerce-ma.js
+++ js/script-mcommerce-ma.js
@@ -184,7 +184,7 @@
       'Cookies policy': 'سياسة الكوكيز',
       'My profile': 'صفحتي الشخصية',
       
-      'https://secure.ikea.com/webapp/wcs/stores/servlet/LogonForm?storeId=55&langId=-83': 'https://secure.ar.ikea.com/ma/ar/webapp/wcs/stores/servlet/LogonForm?storeId=55&langId=-83'
+      'https://www.ikea.com/ma/en/profile/login/': 'https://www.ikea.com/ma/ar/profile/login/'
     };
     const french = {
       'Products': 'Produits',
@@ -216,7 +216,7 @@
       'Cookies policy': 'Politique de cookies',
       'My profile': 'Mon profil',
       
-      'https://secure.ikea.com/webapp/wcs/stores/servlet/LogonForm?storeId=55&langId=-83': 'https://secure.ikea.com/webapp/wcs/stores/servlet/LogonForm?storeId=55&langId=-81'
+      'https://www.ikea.com/ma/en/profile/login/': 'https://www.ikea.com/ma/fr/profile/login/'
     };
 	if (lang == 'fr') {
 		return french[english];
@@ -262,7 +262,7 @@
 
       '<ul class="header__icon-list header__icon-count-3">' +
 
-      '<li class="header__icon-list-item"><a class="header__icon-list-item-link" href="' + __('https://secure.ikea.com/webapp/wcs/stores/servlet/LogonForm?storeId=55&langId=-83') + '"><div class="header__icon-list-item-icon">' +
+      '<li class="header__icon-list-item"><a class="header__icon-list-item-link" href="' + __('https://www.ikea.com/ma/en/profile/login/') + '"><div class="header__icon-list-item-icon">' +
       '<svg class="hnf-svg-icon" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15,12H9a5,5,0,0,0-5,5v4H20V17A5,5,0,0,0,15,12Zm3,7H6V17a3,3,0,0,1,3-3h6a3,3,0,0,1,3,3Zm-6-8A4,4,0,1,0,8,7,4,4,0,0,0,12,11Zm0-6a2,2,0,1,1-2,2A2,2,0,0,1,12,5Z"></path></svg>' +
       '</div><span class="header__icon-list-item-text">' + __('My profile') + '</span></a></li>' +
 
